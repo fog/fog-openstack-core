77c5a092124ad432c11fbb1721a8eb8c24c5c815
Mike Hagedorn <mike@silverchairsolutions.com>
2014-02-18 11:42:35 -0600

    added vcr stuff

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,7 @@
 require 'minitest/autorun'
 require 'minitest/spec'
 require 'minitest/pride'
+
+require File.expand_path './support/vcr_setup.rb', __dir__
+
+MinitestVcr::Spec.configure!
---
+++ spec/support/vcr_setup.rb
@@ -0,0 +1,8 @@
+require 'vcr'
+require "minitest-vcr"
+require "webmock"
+
+VCR.configure do |c|
+    c.cassette_library_dir = 'spec/cassettes'
+    c.hook_into :webmock
+end
