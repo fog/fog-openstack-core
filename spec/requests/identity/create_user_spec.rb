bd7c0a52fe27b69cbdaa6627542ddfd50770d643
Mike Hagedorn <mike@silverchairsolutions.com>
2014-03-03 16:13:32 -0600

    refreshed cassette

--- spec/requests/identity/create_user_spec.rb
+++ spec/requests/identity/create_user_spec.rb
@@ -7,7 +7,7 @@ describe Fog::Identity::OpenStackCommon::Real do
 
   let(:valid_options) { {
     :provider => 'OpenStackCommon',
-    :openstack_auth_url => "http://172.16.0.2:5000/v2.0/tokens",
+    :openstack_auth_url => "http://10.0.1.127:5000/v2.0/tokens",
     :openstack_username => "admin",
     :openstack_api_key => "stack"
     } }
@@ -19,12 +19,12 @@ describe Fog::Identity::OpenStackCommon::Real do
     it "adds a user" do
       name = "jsmith#{Time.now.to_i}"
       password = "password!"
-      tenant_id = "cb7358a6546147a38d13c885626f10f3"
+      tenant_id = "e5d888bd17d941eaa4a9a47a674a9d6d"
       email = "jsmith#{Time.now.to_i}@acme.com"
       enabled = true
 
       result = service.create_user(name, password, email, tenant_id, enabled)
-      result.status.must_include [200,202]
+      result.status.must_equal 200
     end
 
   end
