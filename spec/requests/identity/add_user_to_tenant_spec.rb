59060b139582cb6b2aec92df8003ad73c978484f
Chris Johnson <wchrisjohnson@gmail.com>
2014-03-04 11:14:40 -0500

    make devstack ip address based on devstack.local

--- spec/requests/identity/add_user_to_tenant_spec.rb
+++ spec/requests/identity/add_user_to_tenant_spec.rb
@@ -1,5 +1,4 @@
 require_relative '../../spec_helper'
-require_relative '../../devstack'
 
 require 'fog/openstackcommon'
 
@@ -7,7 +6,7 @@ describe Fog::Identity::OpenStackCommon::Real do
 
   let(:valid_options) { {
     :provider          => 'OpenStackCommon',
-    :openstack_auth_url => "http://#{IP_ADDRESS}:5000/v2.0/tokens",
+    :openstack_auth_url => "http://devstack.local:5000/v2.0/tokens",
     :openstack_username => "admin",
     :openstack_api_key => "stack"
   } }
