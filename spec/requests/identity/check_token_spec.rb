19d3d9974eebe9a1c7e67d43d6eda4375f313437
Chris Johnson <wchrisjohnson@gmail.com>
2014-03-04 13:42:22 -0500

    check_token spec

---
+++ spec/requests/identity/check_token_spec.rb
@@ -0,0 +1,33 @@
+require_relative '../../spec_helper'
+require 'fog/openstackcommon'
+
+describe Fog::Identity::OpenStackCommon::Real do
+
+  let(:valid_options) { {
+    :provider => 'OpenStackCommon',
+    :openstack_auth_url => "http://devstack.local:5000/v2.0/tokens",
+    :openstack_username => "admin",
+    :openstack_api_key => "stack"
+    } }
+
+  let(:service) { Fog::Identity.new(valid_options) }
+
+  describe "#check_token" do
+
+    it "good token", :vcr do
+      token_id = service.auth_token
+      tenant_id = service.current_tenant[:name]
+      result = service.check_token(token_id, tenant_id)
+      [200, 203, 204].must_include result.status
+    end
+
+    it "bad token", :vcr do
+      token_id = "abcdefghijklmnopqrstuvwxyz"
+      tenant_id = "dummy"
+      proc {
+        service.check_token(token_id, tenant_id)
+      }.must_raise Fog::Identity::OpenStackCommon::NotFound
+    end
+
+  end
+end
