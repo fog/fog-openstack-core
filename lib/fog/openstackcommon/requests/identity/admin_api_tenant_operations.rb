c88db0e3ab3e1c0ebe7e69ef7bee370f940a62cd
Chris Johnson <wchrisjohnson@gmail.com>
2014-03-05 15:58:06 -0500

    finished out initial cut at tenant specs

--- lib/fog/openstackcommon/requests/identity/admin_api_tenant_operations.rb
+++ lib/fog/openstackcommon/requests/identity/admin_api_tenant_operations.rb
@@ -16,36 +16,11 @@ module Fog
           )
         end
 
-        # class Mock
-        #   def list_tenants
-        #     Excon::Response.new(
-        #       :body => {
-        #         'tenants_links' => [],
-        #         'tenants' => [
-        #           {'id' => '1',
-        #            'description' => 'Has access to everything',
-        #            'enabled' => true,
-        #            'name' => 'admin'},
-        #           {'id' => '2',
-        #            'description' => 'Normal tenant',
-        #            'enabled' => true,
-        #            'name' => 'default'},
-        #           {'id' => '3',
-        #            'description' => 'Disabled tenant',
-        #            'enabled' => false,
-        #            'name' => 'disabled'}
-        #         ]
-        #       },
-        #       :status => [200, 204][rand(1)]
-        #     )
-        #   end # def list_tenants
-        # end
-
         def get_tenants_by_name(name)
           request(
             :expects  => [200],
             :method   => 'GET',
-            :path     => "tenants?name=#{name}"
+            :path     => "/tenants?name=#{name}"
           )
         end
 
@@ -53,51 +28,17 @@ module Fog
           request(
             :expects  => [200, 204],
             :method   => 'GET',
-            :path     => "tenants/#{tenant_id}"
+            :path     => "/tenants/#{tenant_id}"
           )
         end
 
-        # class Mock
-        #   def get_tenant(id)
-        #     response = Excon::Response.new
-        #     response.status = [200, 204][rand(1)]
-        #     response.body = {
-        #       'tenant' => {
-        #         'id' => id,
-        #         'description' => 'Has access to everything',
-        #         'enabled' => true,
-        #         'name' => 'admin'
-        #       }
-        #     }
-        #     response
-        #   end # def list_tenants
-        # end # class Mock
-
-
-        # request :list_roles_for_user_on_tenant
         def list_roles_for_user_on_tenant(tenant_id, user_id)
           request(
             :expects  => [200],
             :method   => 'GET',
-            :path     => "tenants/#{tenant_id}/users/#{user_id}/roles"
+            :path     => "/tenants/#{tenant_id}/users/#{user_id}/roles"
           )
-        end # def list_roles_for_user_on_tenant
-
-        # class Mock
-        #   def list_roles_for_user_on_tenant(tenant_id, user_id)
-        #     self.data[:user_tenant_membership][tenant_id] ||= {}
-        #     self.data[:user_tenant_membership][tenant_id][user_id] ||= []
-        #     roles = self.data[:user_tenant_membership][tenant_id][user_id].map do |role_id|
-        #       self.data[:roles][role_id]
-        #     end
-        #
-        #     Excon::Response.new(
-        #       :body   => { 'roles' => roles },
-        #       :status => 200
-        #     )
-        #   end # def list_roles_for_user_on_tenant
-        # end # class Mock
-
+        end
 
       end
 
--- spec/requests/identity/admin_api_tenant_operations_spec.rb
+++ spec/requests/identity/admin_api_tenant_operations_spec.rb
@@ -14,28 +14,92 @@ describe Fog::Identity::OpenStackCommon::Real do
 
   describe "#list_tenants", :vcr do
 
-    let(:result) { service.list_tenants }
+  #         'tenants_links' => [],
+  #         'tenants' => [
+  #           {'id' => '1',
+  #            'description' => 'Has access to everything',
+  #            'enabled' => true,
+  #            'name' => 'admin'},
+  #           {'id' => '2',
+  #            'description' => 'Normal tenant',
+  #            'enabled' => true,
+  #            'name' => 'default'},
+  #           {'id' => '3',
+  #            'description' => 'Disabled tenant',
+  #            'enabled' => false,
+  #            'name' => 'disabled'}
+  #         ]
+  #       },
+  #       :status => [200, 204][rand(1)]
+
+    let(:list) { service.list_tenants }
 
     it "lists the tenants" do
-      result.status.must_equal 200
+      [200, 204].must_include list.status
     end
 
     it "returns valid data" do
-      result.body['tenants'].first['id'].wont_be_nil
+      list.body['tenants'].first['id'].wont_be_nil
     end
 
   end
 
-  describe "#get_tenants_by_name", :vcr do
-    it { skip("TBD") }
+  describe "#get_tenants_by_name" do
+
+    let(:list) { service.list_tenants }
+
+    it "gets tenant", :vcr do
+      name = list.body['tenants'].first['name']
+      test_tenant = service.get_tenants_by_name(name)
+      [200, 204].must_include list.status
+    end
+
+    it "returns not found error", :vcr do
+      proc {
+        service.get_tenants_by_name("missingtenant12345")
+      }.must_raise Fog::Identity::OpenStackCommon::NotFound
+    end
+
   end
 
-  describe "get_tenants_by_id", :vcr do
-    it { skip("TBD") }
+  describe "#get_tenants_by_id" do
+
+    let(:list) { service.list_tenants }
+
+    it "gets tenant", :vcr do
+      id = list.body['tenants'].first['id']
+      test_tenant = service.get_tenants_by_id(id)
+      [200, 204].must_include list.status
+    end
+
+    it "returns not found error", :vcr do
+      proc {
+        service.get_tenants_by_id("123456789")
+      }.must_raise Fog::Identity::OpenStackCommon::NotFound
+    end
+
+    #     response.status = [200, 204][rand(1)]
+    #     response.body = {
+    #       'tenant' => {
+    #         'id' => id,
+    #         'description' => 'Has access to everything',
+    #         'enabled' => true,
+    #         'name' => 'admin' } }
   end
 
   describe "#list_roles_for_user_on_tenant", :vcr do
-    it { skip("TBD") }
+
+    let(:list) { service.list_tenants }
+
+    it "gets list of roles", :vcr do
+      tenant_id = list.body['tenants'].first['id']
+      user_id = "2f649419c1ed4801bea38ead0e1ed6ad"
+      result = service.list_roles_for_user_on_tenant(tenant_id, user_id)
+      [200, 204].must_include result.status
+    end
+
+    #       :body   => { 'roles' => roles },
+    #       :status => 200
   end
 
 end
