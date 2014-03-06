a1f9eef09daf9e05d8d1d19be55bdd18335e6b90
Chris Johnson <wchrisjohnson@gmail.com>
2014-03-06 09:38:49 -0500

    get coverage working properly, cruft

--- Rakefile
+++ Rakefile
@@ -1,10 +1,18 @@
-require 'bundler/setup'
-require 'rake'
+# require 'bundler/setup'
+# require 'rake'
 require 'rake/testtask'
 
 Rake::TestTask.new do |t|
+  t.libs.push 'spec'
   t.pattern = 'spec/**/*_spec.rb'
+  t.warning = true
   t.verbose = true
 end
 
 task(default: :test)
+
+desc 'Generates a coverage report'
+task :coverage do
+  ENV['COVERAGE'] = 'true'
+  Rake::Task['test'].execute
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,22 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/spec/"
+end
+
+
 require 'minitest/autorun'
 require 'minitest/spec'
 require 'vcr'
 require "minitest-vcr"
 require "webmock"
 require "minitest/reporters"
-require 'simplecov'
 
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
-  #c.debug_logger = File.open('log/vcr.log', 'w')
-  c.debug_logger = $stdout
+  # c.debug_logger = $stdout
   c.default_cassette_options = { :serialize_with => :json }
 end
 
 MinitestVcr::Spec.configure!
 Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new]
-SimpleCov.start
