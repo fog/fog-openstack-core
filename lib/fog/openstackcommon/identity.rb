17a4ac062810017d049f40fbc36e9531465d84f7
Chris Johnson <wchrisjohnson@gmail.com>
2014-03-07 00:58:57 -0500

    method not implemented in keystone

--- lib/fog/openstackcommon/identity.rb
+++ lib/fog/openstackcommon/identity.rb
@@ -89,7 +89,7 @@ module Fog
       request :update_user
       request :delete_user
       request :enable_user
-      request :list_global_roles_for_user
+      # request :list_global_roles_for_user    see above: :list_user_global_roles
       request :add_global_role_to_user
       request :delete_global_role_for_user
       request :add_credential_to_user
