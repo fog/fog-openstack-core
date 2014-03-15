b80bdf9dd5f65a89e859772d80054d00794e968f
Chris Johnson <wchrisjohnson@gmail.com>
2014-03-15 12:07:15 -0400

    ec2 credentials collection spec

--- lib/fog/openstackcommon/models/identity/ec2_credentials.rb
+++ lib/fog/openstackcommon/models/identity/ec2_credentials.rb
@@ -5,15 +5,13 @@ module Fog
   module Identity
     class OpenStackCommon
       class Ec2Credentials < Fog::Collection
-        model Fog::Identity::OpenStackCommon::Ec2Credential
+        model Fog::Identity::OpenStackCommon::Ec2Credentials
 
         attribute :user
 
         def all
-          user_id = user ? user.id : nil
-
-          ec2_credentials = service.list_ec2_credentials(user_id)
-
+          return [] unless user
+          ec2_credentials = service.list_ec2_credentials(user.id)
           load(ec2_credentials.body['credentials'])
         end
 
@@ -22,7 +20,6 @@ module Fog
             attributes[:user_id]   ||= user.id
             attributes[:tenant_id] ||= user.tenant_id
           end
-
           super attributes
         end
 
@@ -32,22 +29,24 @@ module Fog
         end
 
         def find_by_access_key(access_key)
-          user_id = user ? user.id : nil
-
-          ec2_credential =
-            self.find { |cred| cred.access == access_key }
-
-          unless ec2_credential then
-            response = service.get_ec2_credential(user_id, access_key)
-            body = response.body['credential']
-            body = body.merge 'service' => service
-
-            ec2_credential = Fog::Identity::OpenStackCommon::EC2Credential.new(body)
-          end
-
-          ec2_credential
+          find_credential(access_key) || build_credential(access_key)
         end
-      end
-    end
-  end
-end
+
+        private
+
+        def find_credential(access_key)
+          self.find { |cred| cred.access == access_key }
+        end
+
+        def build_credential(access_key)
+          return nil unless user
+          response_hash = service.get_ec2_credential(user.id, access_key)
+          cred_attribs = response_hash.body['credential']
+          Fog::Identity::OpenStackCommon::EC2Credential.new(
+            cred_attribs.merge('service' => service))
+        end
+
+      end  # Ec2Credentials
+    end  # OpenStackCommon
+  end  # Identity
+end  # Fog
