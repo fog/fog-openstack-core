be226d87d5375a685d31fd2b49480111038af94f
Chris Johnson <wchrisjohnson@gmail.com>
2014-03-15 01:00:52 -0400

    roles spec

--- spec/models/identity/roles_spec.rb
+++ spec/models/identity/roles_spec.rb
@@ -7,13 +7,55 @@ describe "models" do
   describe "identity" do
     describe "roles" do
 
+      let(:service_mock) { Minitest::Mock.new }
+
+      let(:fake_id) { "1234567890" }
+
+      let(:options) { {
+        :service => service_mock
+      } }
+
+      let(:fake_roles_collection) {
+        Fog::Identity::OpenStackCommon::Roles.new(options)
+      }
+
+      let(:fake_roles_response) {
+        response = OpenStruct.new
+        response.body = {'roles' => {}}
+        response
+      }
+
+      before do
+        # have to do this to handle fog-core check
+        service_mock.expect(:nil?, false, [])
+      end
+
+
       describe "#all" do
-        it { skip("TBD") }
+
+        it "gets all roles" do
+          service_mock.expect(:list_roles, fake_roles_response, [])
+
+          fake_roles_collection.all
+        end
+
       end
 
 
       describe "#get" do
-        it { skip("TBD") }
+
+        let(:fake_role_response) {
+          response = OpenStruct.new
+          response.body = {'role' => {}}
+          response
+        }
+
+        it "when role exists in collection" do
+          service_mock.expect(:get_role, fake_role_response, [fake_id])
+
+          fake_roles_collection.get(fake_id)
+        end
+
       end
 
     end
