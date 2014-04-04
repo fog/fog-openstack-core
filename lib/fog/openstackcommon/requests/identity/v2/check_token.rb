62cabbaa90722ba1b6744df81b85bd66493f2c51
Chris Johnson <wchrisjohnson@gmail.com>
2014-04-04 10:38:06 -0400

    tweak approach to params per Kyle - good action

--- lib/fog/openstackcommon/requests/identity/v2/check_token.rb
+++ lib/fog/openstackcommon/requests/identity/v2/check_token.rb
@@ -4,10 +4,14 @@ module Fog
       class Real
 
         def check_token(token_id, tenant_id)
+          params = Hash.new
+          params['belongsTo'] = tenant_id if tenant_id
+
           request(
             :method   => 'HEAD',
             :expects  => [200, 203, 204],
-            :path     => "/v2.0/tokens/#{token_id}?belongsTo=#{tenant_id}", 
+            :path     => "/v2.0/tokens/#{token_id}",
+            :query    => params,
             :admin    => true
           )
         end
