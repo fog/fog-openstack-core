419fe024f619d6c53b5a4ef37040e9642cf89921
Chris Johnson <wchrisjohnson@gmail.com>
2014-04-08 17:25:51 -0400

    cruft

--- lib/fog/openstackcommon/common.rb
+++ lib/fog/openstackcommon/common.rb
@@ -2,13 +2,6 @@ module Fog
   module OpenStackCommon
     module Common
 
-      # CGI.escape, but without special treatment on spaces
-      def self.escape(str,extra_exclude_chars = '')
-        str.gsub(/([^a-zA-Z0-9_.-#{extra_exclude_chars}]+)/) do
-          '%' + $1.unpack('H2' * $1.bytesize).join('%').upcase
-        end
-      end
-
       def self.string_to_class(string)
         string.split('::').inject(Object) do |mod, class_name|
           mod.const_get(class_name)
