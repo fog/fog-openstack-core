4a78359a65c572b28c99db6ab726de117c7f2c3c
Evan Light <evan@tripledogdare.net>
2014-04-16 13:48:30 -0400

    * Change access to a couple of methods to be private * YARD doc'd the public class methods

--- lib/fog/openstackcore/service_discovery.rb
+++ lib/fog/openstackcore/service_discovery.rb
@@ -1,10 +1,8 @@
-# service_discovery.rb
-# --------------------
+# Factory for Fog::OpenStackCore services.  Services must explicitly register 
+# with ServiceDiscovery in order to be available for use.
+#
 # Initially, this class will be used for identity, but no reason it shouldnt
 # be used for service/version discovery across all services in the catalog.
-
-# require 'fog/openstackcore'
-
 module Fog
   module OpenStackCore
     class ServiceDiscovery
@@ -16,44 +14,35 @@ module Fog
 
       attr_accessor :service_identifier  # :identity, :compute, etc.
       attr_accessor :options  # passed in (params)
-      
+
+      # Registers a class as a Fog::Service to be discoverable through 
+      # ServiceDiscovery
+      # @param klass [Class] The Service to register
       def self.register_service(klass)
         assert_namespace_of klass
         @valid_services ||= {}
         @valid_services[registry_name_for(klass)] = klass
       end
 
+      # Unregisters a class from being discoverable.
+      # @param klass [Class] The Service to unregister
       def self.unregister_service(klass)
         @valid_services ||= {}
         @valid_services.delete(registry_name_for(klass))
       end
 
+      # @return [Hash] The registered Service classes keyed by their 
+      # ServiceDiscovery identitier
       def self.valid_services
         @valid_services ||= {}
         @valid_services.dup
       end
 
-      def self.assert_namespace_of(klass)
-        scope = klass.to_s.split(/::/)
-        if scope[0] != "Fog" && scope[1] != "OpenStackCore"
-          raise Fog::OpenStackCore::ServiceError, "#{klass} is not in Fog::OpenStackCore"
-        end
-      end
-
-      def self.class_name_for(klass)
-        klass.to_s.split(/::/).last
-      end
-
-      def self.registry_name_for(klass)
-        class_name_for(klass).downcase
-      end
-
-
-      # -- params --
-      # service identifier (used to look up service in catalog), required
-      # params hash:
-      # - url of service, optional
-      # - service version, optional, but can be stipulated if desired
+      # @params service [String] The name of the service to discover upon. 
+      # This is a downcased String, e.g. "identity" or "storage"
+      # @param params [Hash] Optional parameters including:
+      # * url of service
+      # * the version
       def initialize(service, params = {})
 
         # ToDo: This is a HACKY first cut to get this working...
@@ -68,7 +57,7 @@ module Fog
         @options = params.dup
       end
 
-      # factory - return the service object ready to be used
+      # @return [Fog::Service] Instance of the appropriate type of service
       def call
         service_name = service_identifier.capitalize
         version = options[:version] || DEFAULT_VERSION
@@ -86,6 +75,21 @@ module Fog
       end
 
       private
+
+      def self.assert_namespace_of(klass)
+        scope = klass.to_s.split(/::/)
+        if scope[0] != "Fog" && scope[1] != "OpenStackCore"
+          raise Fog::OpenStackCore::ServiceError, "#{klass} is not in Fog::OpenStackCore"
+        end
+      end
+
+      def self.class_name_for(klass)
+        klass.to_s.split(/::/).last
+      end
+
+      def self.registry_name_for(klass)
+        class_name_for(klass).downcase
+      end
 
       def validate
         # raise an error unless valid service name/id passed in
