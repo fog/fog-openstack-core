a68b0b12460ffb9a63d87d69d35ccef6f5fb3105
Chris Johnson <wchrisjohnson@gmail.com>
2014-04-30 21:52:05 -0400

    cruft

--- lib/fog/openstackcore/common.rb
+++ lib/fog/openstackcore/common.rb
@@ -2,7 +2,7 @@ module Fog
   module OpenStackCore
     module Common
 
-      # ToDo: Move to fog-core 
+      # ToDo: Move to fog-core
       def self.string_to_class(string)
         string.split('::').inject(Object) do |mod, class_name|
           mod.const_get(class_name)
@@ -11,30 +11,31 @@ module Fog
 
       # ToDo: Move to fog-core
       # http://devblog.avdi.org/2009/11/20/hash-transforms-in-ruby/
-	  def self.transform_hash(original, options={}, &block)
-		original.inject({}){|result, (key,value)|
-		  value = if (options[:deep] && Hash === value) 
-		            transform_hash(value, options, &block)
-		          else 
-		            value
-		          end
-		  block.call(result,key,value)
-		  result
-		}
-	  end
+      def self.transform_hash(original, options={}, &block)
+      original.inject({}){|result, (key,value)|
+        value = if (options[:deep] && Hash === value)
+                  transform_hash(value, options, &block)
+                else
+                  value
+                end
+        block.call(result,key,value)
+        result
+      }
+      end
 
-	  # ToDo: Move to fog-core
-	  # Returns a new hash with all keys converted to strings.
-	  def self.stringify_keys(hash)
-	    self.transform_hash(hash) {|hash, key, value|       
-		  hash[key.to_s] = value
-		}
-	  end
+      # ToDo: Move to fog-core
+      # Returns a new hash with all keys converted to strings.
+      def self.stringify_keys(hash)
+        self.transform_hash(hash) {|hash, key, value|
+        hash[key.to_s] = value
+      }
+      end
 
-	  # returns a list of valid keys
-	  def self.whitelist_keys(hash, valid_keys)
-		hash.select {|k,v| valid_keys.include?(k)} 
-	  end
+      # returns a list of valid keys
+      def self.whitelist_keys(hash, valid_keys)
+        valid_hash = self.stringify_keys(hash)
+        valid_hash.select {|k,v| valid_keys.include?(k)}
+      end
 
     end # Common
   end # OpenStackCore
