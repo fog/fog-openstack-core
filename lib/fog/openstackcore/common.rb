5205df247a444ebcff2633c2659c2fe161123a05
Chris Johnson <wchrisjohnson@gmail.com>
2014-05-05 11:35:03 -0400

    replace curly branches with do end per best practices (thanks Kyle)

--- lib/fog/openstackcore/common.rb
+++ lib/fog/openstackcore/common.rb
@@ -12,15 +12,15 @@ module Fog
       # ToDo: Move to fog-core
       # http://devblog.avdi.org/2009/11/20/hash-transforms-in-ruby/
       def self.transform_hash(original, options={}, &block)
-      original.inject({}){|result, (key,value)|
-        value = if (options[:deep] && Hash === value)
-                  transform_hash(value, options, &block)
-                else
-                  value
-                end
-        block.call(result,key,value)
-        result
-      }
+        original.inject({}) do |result, (key,value)|
+          value = if (options[:deep] && Hash === value)
+                    transform_hash(value, options, &block)
+                  else
+                    value
+                  end
+          block.call(result,key,value)
+          result
+        end
       end
 
       # ToDo: Move to fog-core
