4e971e79efbd0af43967063aa591f113ee6c9444
Mike Hagedorn <mike@silverchairsolutions.com>
2014-05-13 13:49:07 -0500

    added filtering

--- spec/requests/compute/v2/image_operations_spec.rb
+++ spec/requests/compute/v2/image_operations_spec.rb
@@ -10,16 +10,25 @@ describe "requests" do
   describe "compute_v2" do
     describe "image operations" do
 
-      let(:admin_options) { admin_options_hash }
+      let(:demo_options) {
+        {
+          :openstack_auth_url => ENV['OS_AUTH_URL'] || "http://devstack.local:5000",
+          :openstack_username => ENV['OS_USER'] || "demo",
+          :openstack_api_key => ENV['OS_API_KEY'] || "stack",
+          :openstack_tenant  => ENV['OS_TENANT'] || "demo",
+          :openstack_region  => ENV['OS_REGION'] || "regionone",
+          :connection_options => {:proxy => 'http://localhost:8888'}
+        }
+      }
 
-      let(:identity) { Fog::OpenStackCore::IdentityV2.new(admin_options) }
+      let(:identity) { Fog::OpenStackCore::IdentityV2.new(demo_options) }
 
       let(:tenant_id) {
         data = identity.get_tenants_by_name(admin_options_hash[:openstack_tenant])
         data.body['tenant']['id']
       }
 
-      let(:service) { Fog::OpenStackCore::ComputeV2.new(admin_options) }
+      let(:service) { Fog::OpenStackCore::ComputeV2.new(demo_options) }
 
       describe "#list_images" do
 
@@ -30,6 +39,53 @@ describe "requests" do
         end
 
       end
+
+      describe "#list_images?changes-since" do
+
+        let(:list) { service.list_images("changes-since" => "2011-01-24T17:08Z") }
+
+        it "returns proper status", :vcr do
+          assert_includes([200, 203], list.status)
+        end
+
+      end
+
+      describe "#list_images?server" do
+        let(:server) { service.list_servers.body["servers"].first["id"] }
+
+        #TODO this doesnt appear to work....
+        let(:list) { service.list_images("server" => server) }
+
+        it "returns proper status", :vcr do
+          assert_includes([200, 203], list.status)
+        end
+
+      end
+
+      describe "#list_images?name" do
+        let(:server) { service.list_servers.body["servers"].first["name"] }
+
+        #TODO this doesnt appear to work....
+        let(:list) { service.list_images("server" => server) }
+
+        it "returns proper status", :vcr do
+          assert_includes([200, 203], list.status)
+        end
+
+      end
+
+      describe "#list_images?type" do
+
+        #TODO this doesnt appear to work....
+        let(:list) { service.list_images("type" => "ALL") }
+
+        it "returns proper status", :vcr do
+          assert_includes([200, 203], list.status)
+        end
+
+      end
+
+
 
     end
   end
