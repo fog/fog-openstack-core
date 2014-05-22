11ff9917383852702f1d6a801b86562be13165b8
Mike Hagedorn <mike@silverchairsolutions.com>
2014-05-22 10:05:57 -0500

    updated spec for new helper

--- spec/requests/compute/v2/addresses_operations_spec.rb
+++ spec/requests/compute/v2/addresses_operations_spec.rb
@@ -9,16 +9,7 @@ require 'fog/openstackcore/services/compute_v2'
 describe "requests" do
   describe "compute_v2" do
     describe "address operations" do
-      let(:demo_options) {
-         {
-           :openstack_auth_url => ENV['OS_AUTH_URL'] || "http://devstack.local:5000",
-           :openstack_username => ENV['OS_USER'] || "demo",
-           :openstack_api_key => ENV['OS_API_KEY'] || "stack",
-           :openstack_tenant  => ENV['OS_TENANT'] || "demo",
-           :openstack_region  => ENV['OS_REGION'] || "regionone",
-           :connection_options => {:proxy => 'http://localhost:8888'}
-         }
-      }
+      let(:demo_options) { demo_options_hash }
 
       let(:identity) { Fog::OpenStackCore::IdentityV2.new(demo_options) }
 
