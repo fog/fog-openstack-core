afb4e87fe44b9947174e4b6b65c04780052ef6fe
Mike Hagedorn <mike@silverchairsolutions.com>
2014-05-30 12:20:23 -0500

    fixed tests

--- spec/requests/compute/v2/server_operations_spec.rb
+++ spec/requests/compute/v2/server_operations_spec.rb
@@ -1,5 +1,5 @@
-require "#{File.dirname(__FILE__)}/../../../../spec_helper"
-require "#{File.dirname(__FILE__)}/../../../../support/spec_helpers"
+require "#{File.dirname(__FILE__)}/../../../spec_helper"
+require "#{File.dirname(__FILE__)}/../../../support/spec_helpers"
 
 include SpecHelpers
 
