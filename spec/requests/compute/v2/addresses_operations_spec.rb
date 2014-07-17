d5a7fc48dd22d04b84a5a2e8a4fcc945469c5380
Mike Hagedorn <mike@silverchairsolutions.com>
2014-07-17 12:51:02 -0400

    added stateful hook

--- spec/requests/compute/v2/addresses_operations_spec.rb
+++ spec/requests/compute/v2/addresses_operations_spec.rb
@@ -17,9 +17,71 @@ describe "requests" do
       let(:image) {service.list_images.body["images"].first["id"]}
       let(:flavor) { service.list_flavors.body["flavors"].first["id"] }
 
+      Minitest.after_run do
+        self.after_run
+      end
+
+      def self.after_run
+        VCR.use_cassette('requests/compute_v2/metadata_operations/server_delete') do
+          puts "cleaning up after server #{self.created_server}"
+          compute_v2_service.delete_server(self.created_server) if TestContext.nova_server
+          TestContext.reset_context
+        end
+      end
+
+      def self.created_server
+        #cache the nova instance so it isnt continually being created
+        TestContext.nova_server do
+          #only fires once
+
+          flavors = service.list_flavors
+          images  = service.list_images
+          server  = service.create_server("#{Time.now.to_i}server",
+                                                     flavors.body["flavors"].first["id"],
+                                                     images.body["images"].first["id"]).body["server"]["id"]
+          #loop until ready
+          begin
+            tries = 7
+            begin
+              if self.query_active(server)
+                puts "Server is UP!"
+              else
+                raise "Server Not Active Yet"
+              end
+            rescue Exception => e
+              tries -= 1
+              puts "Server Not Ready"
+              if tries > 0
+                sleep(10)
+                retry
+              else
+                exit(1)
+              end
+            end
+          end
+          server
+        end
+      end
+
+      def self.query_active(server_id)
+        #return the active servers
+        puts "checking active state of server #{server_id}"
+        response   = compute_v2_service.list_servers(:status => "ACTIVE")
+        active_ids = response.body["servers"].map { |s| s["id"] }
+        unless active_ids.select { |item| item == server_id }.empty?
+          return true
+        end
+        false
+      end
+
+      let(:server) {
+        self.class.created_server
+      }
+      it "exists" do
+        refute_nil(server)
+      end
+
       describe "and a server exists" do
-
-
 
         describe "#list_addresses" do
           before do
